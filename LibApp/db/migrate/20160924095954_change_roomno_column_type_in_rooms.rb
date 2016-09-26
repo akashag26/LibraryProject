fd1a2b20a66ab2b12cebc67dad41052fbfbb32b7
Nitin <coder.neonr@gmail.com>
2016-09-25 22:51:46 -0400

    Updated column

--- LibApp/db/migrate/20160924095954_change_roomno_column_type_in_rooms.rb
+++ LibApp/db/migrate/20160924095954_change_roomno_column_type_in_rooms.rb
@@ -1,6 +1,6 @@
 class ChangeRoomnoColumnTypeInRooms < ActiveRecord::Migration[5.0]
   def up
-    change_column :rooms, :room_no, :integer
+    change_column :rooms, :room_no, 'integer USING CAST("room_no" AS integer)'
   end
   def down
     change_column :rooms, :room_no, :string
