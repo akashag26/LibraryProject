e3af14e90eb3723f08b710348964afee568f1bdb
Nitin <coder.neonr@gmail.com>
2016-09-26 00:15:17 -0400

    Updated booking create method

--- LibApp/app/controllers/bookings_controller.rb
+++ LibApp/app/controllers/bookings_controller.rb
@@ -2,7 +2,7 @@ class BookingsController < ApplicationController
   #before_action :set_booking, only: [:index, :edit, :destroy]
   def index
     @bookings = Booking.all
-    @team  = User.select(:email)
+    @team = User.select(:email)
 
     #render html: 'Inside index'
   end
@@ -11,7 +11,7 @@ class BookingsController < ApplicationController
   # GET /users/1.json
   def show
     @booking = Booking.find(params[:id])
-    @team  = User.select(:email)
+    @team = User.select(:email)
 
   end
 
@@ -22,30 +22,35 @@ class BookingsController < ApplicationController
     @user=@current_user
     @booking = Booking.new
   end
+
   def new1
     @room=Room.find(params[:id])
     @date=params[:date]
     @user=@current_user
     @booking = Booking.new
   end
+
   def new2
     @room=Room.find(params[:id])
     @date=params[:date]
     @user=@current_user
     @booking = Booking.new
   end
+
   def new3
     @room=Room.find(params[:id])
     @date=params[:date]
     @user=@current_user
     @booking = Booking.new
   end
+
   def new4
     @room=Room.find(params[:id])
     @date=params[:date]
     @user=@current_user
     @booking = Booking.new
   end
+
   def new5
     @room=Room.find(params[:id])
     @date=params[:date]
@@ -66,52 +71,56 @@ class BookingsController < ApplicationController
     if @tempuser != nil
       @booking.user=@tempuser.name
 
-    arr = @booking.members.split(',')
-    mymessages = []
-    mymessages.push("Email sent to " +  "#{current_user.email}")
-    mystring = ""
+      mymessages = []
+      if @current_user.kind == 'user'
+        arr = @booking.members.split(',')
+        mymessages.push("Email sent to " + "#{current_user.email}")
+        mystring = ""
 
-    arr.each { |x| if !User.find_by(email: x.strip.downcase)
-                     mystring= "Email Not sent to " +  "#{x.strip.downcase}" + ": Invalid Email "
-                     mymessages.push(mystring)
-                   else
-                     if x.strip.downcase != @current_user.email
-                     mystring= "Email sent to " +  "#{x.strip.downcase}"
-                     mymessages.push(mystring)
-                     end
-                   end}
+        arr.each { |x|
+          if !User.find_by(email: x.strip.downcase)
+            mystring= "Email Not sent to " + "#{x.strip.downcase}" + ": Invalid Email "
+            mymessages.push(mystring)
+          else
+            if x.strip.downcase != @current_user.email
+              mystring= "Email sent to " + "#{x.strip.downcase}"
+              mymessages.push(mystring)
+            end
+          end }
 
-
-    if @booking.save
-      mymessages.push('You have successfully booked a room!')
-      flash[:success] = mymessages.join("<br/>".html_safe)
-      #render html: @booking.room_id
-
-      temp = BookingHistory.new(room_no: @booking.room_no ,
-      user: @booking.user,
-      email: @booking.email,
-      booking_date: @booking.booking_date,
-      startime: @booking.start_time,
-                                endtime: @booking.end_time,
-      building: @booking.building,
-      size: @booking.size,
-       isCancelled: "false",
-      room_id: @booking.room_id)
-      temp.save!
-      if current_user.kind=="user"
-        redirect_to home_path
-      else
-        redirect_to rooms_detailsofroom_path(:room_id => @booking.room_id)
       end
 
+
+      if @booking.save
+        mymessages.push('You have successfully booked a room!')
+        flash[:success] = mymessages.join("<br/>".html_safe)
+        #render html: @booking.room_id
+
+        temp = BookingHistory.new(room_no: @booking.room_no,
+                                  user: @booking.user,
+                                  email: @booking.email,
+                                  booking_date: @booking.booking_date,
+                                  startime: @booking.start_time,
+                                  endtime: @booking.end_time,
+                                  building: @booking.building,
+                                  size: @booking.size,
+                                  isCancelled: "false",
+                                  room_id: @booking.room_id)
+        temp.save!
+        if current_user.kind=="user"
+          redirect_to home_path
+        else
+          redirect_to rooms_detailsofroom_path(:room_id => @booking.room_id)
+        end
+
+      else
+        flash[:error] = 'Unsuccessful Operation!'
+        redirect_to rooms_detailsofroom_path(:room_id => @booking.room_id)
+      end
     else
       flash[:error] = 'Unsuccessful Operation!'
       redirect_to rooms_detailsofroom_path(:room_id => @booking.room_id)
     end
-    else
-      flash[:error] = 'Unsuccessful Operation!'
-      redirect_to rooms_detailsofroom_path(:room_id => @booking.room_id)
-      end
   end
 
   def update
@@ -136,15 +145,15 @@ class BookingsController < ApplicationController
     @booking = Booking.find(params[:id])
     roomid=@booking.room_id
     @booking_history=BookingHistory.find_by(room_no: @booking.room_no, email: @booking.email, booking_date: @booking.booking_date,
-     startime: @booking.start_time);
+                                            startime: @booking.start_time);
     @booking_history.update_attributes(isCancelled: "true")
     @booking.destroy
     #flash[:success] = "User deleted"
     if current_user.kind=="user"
       redirect_to home_path
     else
-    redirect_to rooms_detailsofroom_path(:room_id => roomid)
-      end
+      redirect_to rooms_detailsofroom_path(:room_id => roomid)
+    end
   end
 
   private
@@ -157,12 +166,12 @@ class BookingsController < ApplicationController
   private
 
   def booking_params
-    params.require(:booking).permit( :booking_ID, :user, :email, :start_time, :end_time, :room_no, :booking_date,
-       :building, :size, :room_id, :members)
+    params.require(:booking).permit(:booking_ID, :user, :email, :start_time, :end_time, :room_no, :booking_date,
+                                    :building, :size, :room_id, :members)
   end
 
   def booking_history_params
-    params.require(:booking_history).permit( :room_no, :user, :email, :booking_date, :startime, :building, :size, :room_id)
+    params.require(:booking_history).permit(:room_no, :user, :email, :booking_date, :startime, :building, :size, :room_id)
   end
 
 end
